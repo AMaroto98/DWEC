2d214de92f30e66badd10a115af6ee5873b09ddd
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-10-28 11:24:38 +0200

    Código corregido

--- 05. Tareas del Hogar/Main.js
+++ 05. Tareas del Hogar/Main.js
@@ -1,137 +1,84 @@
 import Persona from './Persona.js';
 
-    // Familia Sastre
-    var padreSastre = new Persona("Pedro", "Sastre", "Sastre", "Padre");
-    var madreSastre = new Persona("Ana", "Sastre", "Sastre", "Madre");
-    var hijoSastre = new Persona("Juan", "Sastre", "Sastre", "Hijo");
-    var hijaSastre = new Persona("Nuria", "Sastre", "Sastre", "Hija");
+// Familia Sastre
+var padreSastre = new Persona("Pedro", "Sastre", "Sastre", "Padre");
+var madreSastre = new Persona("Ana", "Sastre", "Sastre", "Madre");
+var hijoSastre = new Persona("Juan", "Sastre", "Sastre", "Hijo");
+var hijaSastre = new Persona("Nuria", "Sastre", "Sastre", "Hija");
 
-    var familiaSastre = [];
-    var tareasFamiliaSastre = ["Poner lavadora", "Limpiar cocina", "Limpiar baño"];
+var familiaSastre = [];
+var tareasFamiliaSastre = ["Poner lavadora", "Limpiar cocina", "Limpiar baño"];
 
-    familiaSastre.push(padreSastre);
-    familiaSastre.push(madreSastre);
-    familiaSastre.push(hijoSastre);
-    familiaSastre.push(hijaSastre);
+familiaSastre.push(padreSastre);
+familiaSastre.push(madreSastre);
+familiaSastre.push(hijoSastre);
+familiaSastre.push(hijaSastre);
 
-    // crearReparto(familiaSastre, tareasFamiliaSastre, 52);
+crearReparto(familiaSastre, tareasFamiliaSastre, 52);
 
-    // Familia Torrens
-    var padreTorrens = new Persona("Antonio", "Torrens", "Torrens", "Padre");
-    var madreTorrens = new Persona("Estela", "Torrens", "Torrens", "Madre");
-    var hijoTorrens = new Persona("Borja", "Torrens", "Torrens", "Hijo");
-    var hijaTorrens = new Persona("Blanca", "Torrens", "Torrens", "Hija");
+// Familia Torrens
+var padreTorrens = new Persona("Antonio", "Torrens", "Torrens", "Padre");
+var madreTorrens = new Persona("Estela", "Torrens", "Torrens", "Madre");
+var hijoTorrens = new Persona("Borja", "Torrens", "Torrens", "Hijo");
+var hijaTorrens = new Persona("Blanca", "Torrens", "Torrens", "Hija");
 
-    var familiaTorrens = [];
-    var tareasFamiliaTorrens = ["Planchar", "Limpiar baño"];
+var familiaTorrens = [];
+var tareasFamiliaTorrens = ["Planchar", "Limpiar baño"];
 
-    familiaTorrens.push(padreTorrens);
-    familiaTorrens.push(madreTorrens);
-    familiaTorrens.push(hijoTorrens);
-    familiaTorrens.push(hijaTorrens);
+familiaTorrens.push(padreTorrens);
+familiaTorrens.push(madreTorrens);
+familiaTorrens.push(hijoTorrens);
+familiaTorrens.push(hijaTorrens);
 
-    // crearReparto(familiaTorrens, tareasFamiliaTorrens, 4);
+crearReparto(familiaTorrens, tareasFamiliaTorrens, 4);
 
-    // Familia Moll
-    var padreMoll = new Persona("Jaume", "Moll", "Moll", "Padre");
-    var madreMoll = new Persona("Estela", "Moll", "Moll", "Madre");
-    var hijoMoll = new Persona("Alejandro", "Moll", "Moll", "Hijo");
-    var hijaMayorMoll = new Persona("Marga", "Moll", "Moll", "Hija Mayor");
-    var hijaMenorMoll = new Persona("Maria", "Moll", "Moll", "Hija Menor");
-
-
-    var familiaMoll = [];
-    var tareasFamiliaMoll = ["Ponerlavadora", "Limpiar cocina", "Limpiar comedor", "Planchar", "Limpiar baño" ];
-
-    familiaMoll.push(padreMoll);
-    familiaMoll.push(madreMoll);
-    familiaMoll.push(hijoMoll);
-    familiaMoll.push(hijaMayorMoll);
-    familiaMoll.push(hijaMenorMoll);
-
-    crearReparto(familiaMoll, tareasFamiliaMoll, 12);
+// Familia Moll
+var padreMoll = new Persona("Jaume", "Moll", "Moll", "Padre");
+var madreMoll = new Persona("Estela", "Moll", "Moll", "Madre");
+var hijoMoll = new Persona("Alejandro", "Moll", "Moll", "Hijo");
+var hijaMayorMoll = new Persona("Marga", "Moll", "Moll", "Hija Mayor");
+var hijaMenorMoll = new Persona("Maria", "Moll", "Moll", "Hija Menor");
 
 
-    function crearReparto(familia, tareas, duracion) {
-        
-        if (familia.length > tareas.length) {
-            
-            var tabla = "<table><tr><th>Semana</th><th>Persona</th><th>Tarea</th>";
-            var contenido = "";
+var familiaMoll = [];
+var tareasFamiliaMoll = ["Poner lavadora", "Limpiar cocina", "Limpiar comedor", "Planchar", "Limpiar baño"];
 
-            document.getElementById("titulo").innerHTML = "Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n";
+familiaMoll.push(padreMoll);
+familiaMoll.push(madreMoll);
+familiaMoll.push(hijoMoll);
+familiaMoll.push(hijaMayorMoll);
+familiaMoll.push(hijaMenorMoll);
 
-            // console.log("Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n");
-            var indiceTarea = 0;
+crearReparto(familiaMoll, tareasFamiliaMoll, 12);
 
-            for (let semana = 1; semana <= duracion; semana++) {
-                var asignacionSemanal = "";
-        
-                for (let i = 0; i < familia.length; i++) {
-                    var persona = familia[i];
-                    var nombre = persona.getNombre();
-                    var apellido = persona.getApellido();
-                    
-                    var tarea = tareas[indiceTarea];
-                    indiceTarea = (indiceTarea + 1) % tareas.length;
-        
-                    asignacionSemanal += " " + nombre + " " + apellido + " Tarea: " + tarea + "\n";
-                    contenido += "<tr><td>" + semana + "</td><td>" + nombre + " " + apellido + "</td>" + "<td>" + tarea + "</td></tr>";  
-                }
-        
-                // console.log(asignacionSemanal);
-            }
 
-            tabla = tabla + contenido + "</table>"
-            document.getElementById("tabla").innerHTML = tabla;
+function crearReparto(familia, tareas, duracion) {
 
-        } else {
+    document.getElementById("titulo").innerHTML = "Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n";
 
-            document.getElementById("titulo").innerHTML = "Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n";
+    console.log("Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n");
 
-            // console.log("Reparto de tareas de la familia: " + familia[0].getFamilia() + "\n");
+    var tabla = "<table><tr><th>Semana</th><th>Persona</th><th>Tarea</th>";
+    var contenido = "";
 
-            var tabla = "<table><tr><th>Semana</th><th>Persona</th><th>Tarea</th>";
-            var contenido = "";
+    for (let semana = 1; semana <= duracion; semana++) {
 
-            for (let semana = 1; semana <= duracion; semana++) {
+        var asignacionSemanal = "";
 
-                var asignacionSemanal = "";
+        for (let i = 0; i < familia.length; i++) {
+            var persona = familia[i];
+            var nombre = persona.getNombre();
+            var apellido = persona.getApellido();
+            var indiceTarea = (semana + i) % tareas.length;
+            var tarea = tareas[indiceTarea];
 
-                for (let i = 0; i < familia.length; i++) {
-                    var persona = familia[i];
-                    var nombre = persona.getNombre();
-                    var apellido = persona.getApellido();
-                    var indiceTarea = (semana + i) % familia.length;
-                    var tarea = tareas[indiceTarea];
-
-                    asignacionSemanal += " " + nombre + " " + apellido + " Tarea: " + tarea + "\n";
-                    contenido += "<tr><td>" + semana + "</td><td>" + nombre + " " + apellido + "</td>" + "<td>" + tarea + "</td></tr>";  
-                }
-
-                // console.log(asignacionSemanal);               
-            }
-            tabla = tabla + contenido + "</table>"
-            document.getElementById("tabla").innerHTML = tabla;
-
+            asignacionSemanal += " " + nombre + " " + apellido + " Tarea: " + tarea + "\n";
+            contenido += "<tr><td>" + semana + "</td><td>" + nombre + " " + apellido + "</td>" + "<td>" + tarea + "</td></tr>";
         }
+
+        console.log(asignacionSemanal);               
     }
+    tabla = tabla + contenido + "</table>"
+    document.getElementById("tabla").innerHTML = tabla;
 
-    // function crearReparto(familia, tareas, duracion) {
-
-    //     console.log("Planificación de la familia: " + familia[0].getFamilia());
-
-    //     for (let semana = 1; semana <= duracion; semana++) {
-
-    //         var asignacionSemanal = "";
-
-    //         for (let i = 0; i < tareas.length; i++) {
-    //             var tarea = tareas[i];
-    //             var indiceMiembro = (semana + i) % familia.length;
-    //             var miembro = familia[indiceMiembro];
-    //             asignacionSemanal += miembro.getNombre() + " " + miembro.getApellido() + " Tarea: " + tarea + "\n";
-    //         }
-
-    //         console.log(asignacionSemanal);               
-    //     }
-    // }
+}
